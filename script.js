1f02f8680ba911aa256c51c6b326b7f1ed5715a7
bhaskarajmeera <bhaskar.ajmeera@zohomail.com.au>
2026-04-21 09:48:08 +1000

    slide to apps screen is functionality is completed and working

---
+++ script.js
@@ -0,0 +1,23 @@
+/* slide to go to apps screen    */
+
+
+const slider=document.getElementById("mySlider");
+slider.addEventListener('change',(e)=>{
+    const {value}=e.target;
+    console.log(value);
+    const label=document.getElementById("label");
+
+    if(value>70){
+label.textContent = "";
+displayAppScreen();
+    }else{
+        label.textContent = "slide To Unlock";
+    }
+});
+/* hide home screen and show appps screen */
+const displayAppScreen=()=>{
+document.querySelector(".homeScreen").remove();
+
+document.querySelector(".appScreen").style.display = "block";
+
+};
